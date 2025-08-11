7ebc349eb446fbd5c47e4bf3b794f1bc922fed52
ferreiradev7 <humbertopferreiraf@gmail.com>
2025-08-11 13:50:33 -0300

    feat: structs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
 
+typedef struct Animal{
+     int codigoAnimal;
+     char nome[50];
+     char especie[30];
+     char dataNascimento[10];
+};
+
+typedef struct Veterinario{
+    int crmVet;
+    char nome[50];
+    char telefone[15];
+
+};
+
+typedef struct Consulta{
+    int codigoConsulta;
+    int codigoAnimal;
+    int crmVet;
+};
+
+
+
+
+
+
+
 
 
 
